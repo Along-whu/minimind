55125a921744beb9143c697ed51f118b41f6a9d1
caolong-whu <970981298@qq.com>
2026-03-04 16:50:16 +0800

    add sft datasets and trainer

--- dataset/lm_dataset.py
+++ dataset/lm_dataset.py
@@ -3,6 +3,37 @@ import os
 import torch
 from torch.utils.data import Dataset
 from datasets import load_dataset
+import random
+
+
+def pre_processing_chat(conversations, add_system_ratio=0.2):
+    SYSTEM_PROMPTS = [
+        "你是一个知识丰富的AI，尽力为用户提供准确的信息。",
+        "你是minimind，一个小巧但有用的语言模型。",
+        "你是一个专业的AI助手，请提供有价值的回答。",
+        "你是minimind，请尽力帮助用户解决问题。",
+        "你是一个可靠的AI，请给出准确的回答。",
+        "You are a helpful AI assistant.",
+        "You are minimind, a lightweight intelligent assistant.",
+        "You are a friendly chatbot. Please answer the user's questions carefully.",
+        "You are a knowledgeable AI. Try your best to provide accurate information.",
+        "You are minimind, a small but useful language model.",
+    ]
+    if conversations[0] and conversations[0].get("role") != "system":
+        if random.random() < add_system_ratio:
+            return [
+                {"role": "system", "content": random.choice(SYSTEM_PROMPTS)}
+            ] + conversations
+    return conversations
+
+
+def post_processing_chat(prompt_content, empty_think_ratio=0.05):
+    if (
+        "<think>\n\n<think>\n\n" in prompt_content
+        and random.random() > empty_think_ratio
+    ):
+        prompt_content = prompt_content.replace("<think>\n\n<think>\n\n", "")
+    return prompt_content
 
 
 class PretrainedDataset(Dataset):
@@ -33,3 +64,72 @@ class PretrainedDataset(Dataset):
         ] = -100  # Ignore padding tokens in loss computation
 
         return input_ids, labels
+
+
+class SFTDataset(Dataset):
+    def __init__(self, jsonl_path, tokenizer, max_length=1024):
+        super().__init__()
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.samples = load_dataset(jsonl_path, data_files=jsonl_path, split="train")
+        self.bos_id = tokenizer(
+            f"{tokenizer.bos_id}assistan\n", add_special_tokens=False
+        ).input_ids
+        self.eos_id = tokenizer(
+            f"{tokenizer.eos_id}\n", add_special_tokens=False
+        ).input_ids
+
+    def creat_chat_prompt(self, conversations):
+        messages = conversations.copy()
+        tools = (
+            conversations[0]["functions"]
+            if (
+                conversations[0]
+                and conversations[0]["role"] == "system"
+                and conversations[0].get("functions")
+            )
+            else None
+        )
+        return self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=False,
+            tools=tools,
+        )
+
+    def generate_labels(self, input_ids):
+        labels = [-100] * len(input_ids)
+        i = 0
+        while i < len(input_ids):
+            if input_ids[i : i + len(self.bos_id)] == self.bos_id:
+                start = i + len(self.bos_id)
+                end = start
+                while end < len(input_ids):
+                    if input_ids[end : end + len(self.eos)] == self.eos_id:
+                        break
+                    end += 1
+                for j in range(start, min(end + len(self.eos_id), self.max_length)):
+                    labels[j] = input_ids[j]
+                i = end + len(self.eos_id) if end < len(input_ids) else len(input_ids)
+            else:
+                i += 1
+        return labels
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, index):
+        sample = self.samples[index]
+        # 随机插入system prompts
+        conversations = pre_processing_chat(sample)
+        # 将json数据格式转换为文本字符串
+        prompt = self.creat_chat_prompt(conversations)
+        # 将空白思考区删除
+        prompt = post_processing_chat(prompt)
+        input_ids = self.tokenizer(prompt).input_ids[: self.max_length]
+        input_ids += [self.tokenizer.pad_token_id] * (self.max_length - len(input_ids))
+        labels = self.generate_labels(input_ids)
+
+        return torch.tensor(input_ids, dtype=torch.long), torch.tensor(
+            labels, dtype=torch.long
+        )
